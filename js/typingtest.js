398d6c819fe276dd448fa263fa09daf44df8a05e
Emily M <35444424+mackayem@users.noreply.github.com>
2021-11-05 18:31:33 -0700

    adjusts padding for dark/light mode for profile stats

--- js/typingtest.js
+++ js/typingtest.js
@@ -247,16 +247,15 @@ jQuery(document).ready(function($){
         updateQuote();
     });
 
-
-    // $("#pf-lessonSelector__button--darkmode").on("click", toggleDarkMode());
-    // $("#pf-statsProfile__button--darkmode-top").on("click", toggleDarkMode());
-    // $("#pf-statsProfile__button--darkmode-bottom").on("click", toggleDarkMode());
-
     // function toggleDarkMode() {
     //     $("#pf-helpModal__container").toggleClass("pf-darkmode");
     //     $("#pf-typingTest__container").toggleClass("pf-darkmode");
     //     $("#user-profile-container").toggleClass("pf-darkmode");
     // }
+
+    // $("#pf-lessonSelector__button--darkmode").on("click", toggleDarkMode());
+    // $("#pf-statsProfile__button--darkmode-top").on("click", toggleDarkMode());
+    // $("#pf-statsProfile__button--darkmode-bottom").on("click", toggleDarkMode());
 
     $("#pf-lessonSelector__button--darkmode").on("click", function() {
         $("#pf-helpModal__container").toggleClass("pf-darkmode");
